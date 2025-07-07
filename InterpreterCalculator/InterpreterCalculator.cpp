10afe5a36ef1707b691209ed5befa45b3a8e9a5b
Nina Rosliakova <nrosliakova@kse.org.ua>
2025-07-08 01:24:30 +0300

    tokenization func

--- InterpreterCalculator/InterpreterCalculator.cpp
+++ InterpreterCalculator/InterpreterCalculator.cpp
@@ -1,8 +1,51 @@
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
 using namespace std;
+
+vector<string> tokenization(string equation);
 
 int main()
 {
-    cout << "Hello World!\n";
+    string equation;
+    getline(cin, equation);
+
+    vector<string> tokens = tokenization(equation);
+    cout << "Tokens: ";
+    for (int i = 0; i < tokens.size(); i++)
+        cout << tokens[i] << ", ";
+    cout << endl;
+}
+
+vector<string> tokenization(string equation) {
+    string numbers = "1234567890";
+    string operators = "!()+-*/^%";
+    stringstream num;
+    vector<string> tokens;
+    for (auto symb : equation) {
+        if (numbers.find(symb) != string::npos) {
+            num << symb;
+        }
+        else if (operators.find(symb) != string::npos) {
+            if (num.str().length() != 0) {
+                tokens.push_back(num.str());
+                num.str("");
+            }
+            tokens.push_back(string{ symb });
+        }
+        else if (symb == ' ') {
+            if (num.str().length() != 0) {
+                tokens.push_back(num.str());
+                num.str("");
+            }
+        }
+    }
+    if (num.str().length() != 0) {
+        tokens.push_back(num.str());
+        num.str("");
+    }
+    return tokens;
 }
